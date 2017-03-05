e38027c6a82449195e897b447018de353f5c489b
Mitchell Rodríguez <mitrodle@gmail.com>
2017-03-05 18:26:04 -0500

    implementando panel : contador de victorias

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,12 @@
 
 	var x = "x";
 	var o = "o";
-	var turno;
+	var vacio = "+";
+	var turno = o
+	var xGanadas = 0;
+	var oGanadas = 0;
 
-function juego(clicked_value, clicked_id){
+function juego(clicked_id){
 
 	var b1 = document.getElementById("b1")
 	var b2 = document.getElementById("b2")
@@ -15,6 +18,8 @@ function juego(clicked_value, clicked_id){
 	var b7 = document.getElementById("b7")
 	var b8 = document.getElementById("b8")
 	var b9 = document.getElementById("b9")
+	var xWins = document.getElementById("xWins");
+	var oWins = document.getElementById("oWins");
 
 	//Si turno == x, turno = o, de otro modo turno = x
 	//Cumple con que X inicie la partida
@@ -63,6 +68,17 @@ function juego(clicked_value, clicked_id){
 		b9.style.color = "#fff"
 	}
 
+	function contador(){
+
+		if(xGana == true){
+			xGanadas = xGanadas + 1
+			xWins.innerHTML = xGanadas
+		}else if(oGana == true){
+			oGanadas = oGanadas + 1
+			oWins.innerHTML = oGanadas
+		}
+	}
+
 	//Todos los casos en los que jugador X gana
 
 	if((b1.value == x && b2.value == x && b3.value == x) ||
@@ -75,6 +91,7 @@ function juego(clicked_value, clicked_id){
 		(b3.value == x && b5.value == x && b7.value == x)){
 		alert("Jugador X gana")
 		var xGana = true;
+		contador();
 
 	//Todos los casos en jugador O gana
 
@@ -88,6 +105,7 @@ function juego(clicked_value, clicked_id){
 		(b3.value == o && b5.value == o && b7.value == o)){
 		alert("Jugador O gana")
 		var oGana = true;
+		contador();
 	}
 
 	//Nota: Si xGana u oGana son true, dependiendo del if de condiciones para jugadas ganadoras
@@ -103,19 +121,75 @@ function juego(clicked_value, clicked_id){
 		b7.removeAttribute("onclick", "button")
 		b8.removeAttribute("onclick", "button")
 		b9.removeAttribute("onclick", "button")
-		alert("Partida finalizada")
 	}else if((b1.value == "x" || b1.value == "o") && (b2.value == "x" || b2.value == "o") &&
 			(b3.value == "x" || b3.value == "o") && (b4.value == "x" || b4.value == "o") &&
 			(b5.value == "x" || b5.value == "o") && (b6.value == "x" || b6.value == "o") &&
 			(b7.value == "x" || b7.value == "o") && (b8.value == "x" || b8.value == "o") &&
 			(b9.value == "x" || b9.value == "o")){
 		alert("Es un empate")
-		alert("Partida finalizada")
 	}
 }
 
+
+/*function contador(){
+
+	if(xGana == true){
+		contadorBoton = xGanadas + 1
+		alert(contadorBoton)
+	}else if(oGana == true){
+		contadorBoton = oGanadas + 1
+		alert(contadorBoton)
+	}
+}
+*/
+
 //Recargar al hacer click en el botón de Nueva partida
 
-function nuevaPartida(){ 
-    window.location.reload(); 
-} 
+function nuevaPartida(){
+
+	b1.value = vacio
+	b1.style.color = "#ccc"
+	b1.setAttribute("onclick", "juego(this.id)")
+	b1.removeAttribute("disabled", "disabled")
+
+	b2.value = vacio
+	b2.style.color = "#ccc"
+	b2.setAttribute("onclick", "juego(this.id)")
+	b2.removeAttribute("disabled", "disabled")	
+
+	b3.value = vacio
+	b3.style.color = "#ccc"
+	b3.setAttribute("onclick", "juego(this.id)")
+	b3.removeAttribute("disabled", "disabled")	
+
+	b4.value = vacio
+	b4.style.color = "#ccc"
+	b4.setAttribute("onclick", "juego(this.id)")
+	b4.removeAttribute("disabled", "disabled")		
+
+	b5.value = vacio
+	b5.style.color = "#ccc"
+	b5.setAttribute("onclick", "juego(this.id)")
+	b5.removeAttribute("disabled", "disabled")	
+
+	b6.value = vacio
+	b6.style.color = "#ccc"
+	b6.setAttribute("onclick", "juego(this.id)")
+	b6.removeAttribute("disabled", "disabled")	
+
+	b7.value = vacio
+	b7.style.color = "#ccc"
+	b7.setAttribute("onclick", "juego(this.id)")
+	b7.removeAttribute("disabled", "disabled")	
+
+	b8.value = vacio
+	b8.style.color = "#ccc"
+	b8.setAttribute("onclick", "juego(this.id)")
+	b8.removeAttribute("disabled", "disabled")	
+
+	b9.value = vacio
+	b9.style.color = "#ccc"
+	b9.setAttribute("onclick", "juego(this.id)")
+	b9.removeAttribute("disabled", "disabled")
+
+}
